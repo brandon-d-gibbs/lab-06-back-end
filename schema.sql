6e8fc001483f5494e777d0d643a3ec6906a0db76
lamsharkie <gibbs.brandon.d@gmail.com>
2020-02-21 11:22:38 -0800

    sending location data if in DB, inserting if not.

---
+++ schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS locations;
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    search_query VARCHAR(255),
+    formatted_query VARCHAR(255),
+    latitude NUMERIC(10, 7),
+    longitude NUMERIC(10, 7)
+  );
+
+--   INSERT INTO locaions (id, search_query, formatted_query, latitude, longitude) VALUES();
